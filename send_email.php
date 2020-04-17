1910253498391f6014aecb533709e15c0a40dfd1
Eero Kuusisto <thekraken@lakka.kapsi.fi>
2020-04-17 18:32:22 +0300

    Added E-Mail form

---
+++ send_email.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+      <link rel="stylesheet" href="stylesheets/bootstrap.min.css">
+      <link rel="stylesheet" href="stylesheets/htmlq.css">
+</head>
+
+<body>
+<div class="container">
+<p><center>Vielen Dank! Ihre E-Mail-Adresse wurde gespeichert.<br><br>
+Sie können das Fenster jetzt schließen.</center></p>
+</div>
+<body>
+
+<?php
+$to_email = 'eero@kuusisto.de';
+$subject = 'Verlosung';
+$message = $_POST["email"];
+$headers = 'From: eero@kuusisto.de';
+mail($to_email,$subject,$message,$headers);
+?>
+
+</html>
